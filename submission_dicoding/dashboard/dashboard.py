33d37a06e8890547fdf5c3cdd88ffb507174c790
teduhafr <teduhafr@gmail.com>
2023-11-21 10:58:41 +0800

    siap_upload

--- submission_dicoding/dashboard/dashboard.py
+++ submission_dicoding/dashboard/dashboard.py
@@ -20,8 +20,12 @@ data_awal = 'PM2.5'
 
 ##untuk fungsi-fungsi
 def filter_tahun(df, awal, akhir):
-    df_tahun = df.drop(df[(df.year < awal) | (df.year > akhir)].index)
-    return df_tahun
+    if(awal == akhir):
+        df_tahun = df.loc[df["year"] == awal]
+        return df_tahun
+    else:
+        df_tahun = df.drop(df[(df.year < awal) | (df.year > akhir)].index)
+        return df_tahun
 
 
 def data_station_pertahun(df, awal, akhir):
@@ -45,6 +49,10 @@ def data_perjam(df, awal, akhir):
     df_data1 = filter_tahun(df, awal, akhir)
     data_pivot_perjam = df_data1.pivot_table(['PM2.5', 'PM10'], 'hour', aggfunc='mean').reset_index()
     return data_pivot_perjam
+def kota_perjam(df, awal, akhir):
+    df_data2 = filter_tahun(df, awal, akhir)
+    data_pivot_kotaperjam = df_data2.pivot_table(['PM2.5', 'PM10'], ['hour', 'yearmonth', 'station', 'year', 'waktu'], aggfunc='mean').reset_index()
+    return data_pivot_kotaperjam
 
 with st.sidebar:
     #judul
@@ -75,6 +83,7 @@ with st.sidebar:
         option_akhir = st.selectbox(
         'Akhir',
         (tahun_option_akhir))
+
         tahun_akhir = option_akhir
     st.text("Data Terakhir 2017 Februari")
 #buat judul
@@ -82,19 +91,24 @@ st.title('Data PM2.5 dan PM 10'
           + '\n untuk Station Aotizhongxin, Changping, Dingling, Dongsi dan Guanyuan')
 
 ##
-st.subheader('Pemamantauan Kualitas PM2.5 dan PM 10 di Tiap Station', divider='rainbow')
-st.subheader('Kualitas Udara '+ str(data_awal)+ ' Tahun ' + str(tahun_awal) + ' - ' + str(tahun_akhir))
-df = data_station_pertahun(all_df, tahun_awal, tahun_akhir)
-plt.figure(figsize=(13, 5))
-plt.plot(df['year'], df['Aotizhongxin'], label = 'Aotizhongxin', color='red')
-plt.plot(df['year'], df['Changping'], label = 'Changping', color='blue')
-plt.plot(df['year'], df['Dingling'], label = 'Dingling', color='yellow')
-plt.plot(df['year'], df['Dongsi'], label = 'Dongsi', color='black')
-plt.plot(df['year'], df['Guanyuan'], label = 'Guanyuan', color='pink')
-plt.xlabel('tahun',size= 20)
-plt.ylabel('Kadar ' + str(data_awal), size = 20)
-plt.legend()
-st.pyplot(plt)
+if (tahun_awal == tahun_akhir):
+    st.write('Lihat detail di bawah untuk data per tahun')
+    st.divider()
+else:
+    st.subheader('Pemamantauan Kualitas PM2.5 dan PM 10 di Tiap Station', divider='rainbow')
+    st.subheader('Kualitas Udara '+ str(data_awal)+ ' Tahun ' + str(tahun_awal) + ' - ' + str(tahun_akhir))
+    df = data_station_pertahun(all_df, tahun_awal, tahun_akhir)
+    plt.figure(figsize=(13, 5))
+    plt.plot(df['year'], df['Aotizhongxin'], label = 'Aotizhongxin', color='red')
+    plt.plot(df['year'], df['Changping'], label = 'Changping', color='blue')
+    plt.plot(df['year'], df['Dingling'], label = 'Dingling', color='yellow')
+    plt.plot(df['year'], df['Dongsi'], label = 'Dongsi', color='black')
+    plt.plot(df['year'], df['Guanyuan'], label = 'Guanyuan', color='pink')
+    plt.xlabel('tahun',size= 20)
+    plt.ylabel('Kadar ' + str(data_awal), size = 20)
+    plt.legend()
+    st.pyplot(plt)
+
 with st.expander("Detail Per Tahun"):
 
 
@@ -130,3 +144,35 @@ plt.xlabel('Jam',size= 15)
 plt.ylabel('Kadar ' +str(data_awal), size = 15)
 plt.xticks(rotation='vertical')
 st.pyplot(plt)
+
+st.divider()
+st.markdown(''' Jam kita klasifikan seperti berikut untuk mempermudah tampilan
+| Jam | Waktu |
+| ------ | ------ |
+| 19.00 - 04.00 | malam :crescent_moon: |
+| 05.00 - 10.00 | pagi :sunrise: |
+| 11.00 - 15.00 | siang :sun_with_face: |
+| 16.00 - 418.00 | sore :city_sunset: |''')
+st.divider()
+
+st.subheader('Kualitas Udara ' + str(data_awal)+' Per Kota Berdasarkan Waktu '+  ' Tahun ' + str(tahun_awal) + ' - ' + str(tahun_akhir))
+if (data_awal == 'PM2.5') :
+    df_kotaperjam = kota_perjam(all_df, tahun_awal, tahun_akhir)
+    plt.figure(figsize=(7, 3))
+    sns.barplot(data=df_kotaperjam, x = 'station', y = 'PM2.5', hue = 'waktu')
+    plt.rcParams.update({'font.size': 20})
+    plt.legend()
+    plt.xlabel('Kota')
+    plt.ylabel('Kadar PM2.5')
+    st.pyplot(plt)
+elif (data_awal == 'PM10') :
+    df_kotaperjam = kota_perjam(all_df, tahun_awal, tahun_akhir)
+    plt.figure(figsize=(7, 3))
+    sns.barplot(data=df_kotaperjam, x = 'station', y = 'PM10', hue = 'waktu')
+    plt.rcParams.update({'font.size': 20})
+    plt.legend()
+    plt.xlabel('Kota')
+    plt.ylabel('Kadar PM10')
+    st.pyplot(plt)
+else :
+    st.subheader('Tidak ada pilihan')
